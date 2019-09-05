e4634cd154d4295f9b3d011a457a5446ad484ff8
salvadora <salvadora@ZAM-ID-SAAC>
2019-09-05 16:00:42 -0500

    Actualización prototype Main

--- src/main/java/mx/com/msc/mains/PrototypeMain.java
+++ src/main/java/mx/com/msc/mains/PrototypeMain.java
@@ -5,12 +5,31 @@ import mx.com.msc.prototype.TvPrototype;
 
 public class PrototypeMain {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         TvPrototype tvp = new TvPrototype();
-        TV tv = (TV) tvp.prototipo("Plasma");
-        System.out.println(tv.getPrecio());
-        System.out.println(tv.getColor());
-        System.out.println(tv.getMarcas());
+
+        System.out.println("----------------------Clone 1 de Tv Plasma-----------------------");
+        TV tvPlasma = (TV) tvp.prototipo("Plasma");
+        System.out.println(tvPlasma);
+        System.out.println(tvPlasma.getPrecio());
+        System.out.println(tvPlasma.getColor());
+        System.out.println(tvPlasma.getMarcas());
+
+        System.out.println("---------------------Clone 1 de Tv LCD---------------------------");
+
+        TV tvLcd = (TV) tvp.prototipo("LCD");
+        System.out.println(tvLcd);
+        System.out.println(tvLcd.getPrecio());
+        System.out.println(tvLcd.getColor());
+        System.out.println(tvLcd.getMarcas());
+
+        System.out.println("---------------------Clone 2 de Tv LCD---------------------------");
+
+        TV tvLcd2 = (TV) tvp.prototipo("LCD");
+        System.out.println(tvLcd2);
+        System.out.println(tvLcd2.getPrecio());
+        System.out.println(tvLcd2.getColor());
+        System.out.println(tvLcd2.getMarcas());
     }
 
 }
